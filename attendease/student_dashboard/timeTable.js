6d47216762e260e9834fc6b20e8046b629076322
piyushchandola2005 <piyushchandola2005@gmail.com>
2024-11-27 00:33:10 +0530

    create

---
+++ attendease/student_dashboard/timeTable.js
@@ -0,0 +1,215 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '08:40-10:00 AM',
+        roomNumber: '117',
+        subject: 'DAA',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:00-11:00 AM',
+        roomNumber: '117',
+        subject: 'DBMS',
+        type: 'Lecture'
+    },
+    {   
+
+        time: '11:10-12:15 AM',
+        roomNumber: '215/216',
+        subject: 'DBMS(G1)/DAA(G2)',
+        type: 'Lab'
+    },
+      {   
+        time: '12:15-01:05 PM',
+        roomNumber: '   ',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {   
+        time: '01:05-02:10 PM',
+        roomNumber: '117',
+        subject: 'AI',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:10-03:15 PM',
+        roomNumber: '117',
+        subject: 'Soft Computing',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '08:40-10:00 AM',
+        roomNumber: '117',
+        subject: 'Soft Computing',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:00-11:00 AM',
+        roomNumber: '117',
+        subject: 'DAA',
+        type: 'Lecture'
+    },
+    {   
+
+        time: '11:10-12:15 AM',
+        roomNumber: '215/216',
+        subject: 'DBMS(G2)/DAA(G1)',
+        type: 'Lab'
+    },
+      {   
+        time: '12:15-01:05 PM',
+        roomNumber: '   ',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {   
+        time: '01:05-02:10 PM',
+        roomNumber: '117',
+        subject: 'OOSD',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:10-03:15 PM',
+        roomNumber: '117',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '08:40-10:00 AM',
+        roomNumber: '117',
+        subject: 'Soft Skills',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:00-11:00 AM',
+        roomNumber: '117',
+        subject: 'DBMS',
+        type: 'Lecture'
+    },
+    {   
+
+        time: '11:10-12:15 AM',
+        roomNumber: '215/216',
+        subject: 'AI(G1)/Mini Project(G2)',
+        type: 'Lab'
+    },
+      {   
+        time: '12:15-01:05 PM',
+        roomNumber: '   ',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {   
+        time: '01:05-02:10 PM',
+        roomNumber: '117',
+        subject: 'DAA',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:10-03:15 PM',
+        roomNumber: '117',
+        subject: 'OOSD',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '08:40-10:00 AM',
+        roomNumber: '117',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:00-11:00 AM',
+        roomNumber: '117',
+        subject: 'DBMS',
+        type: 'Lecture'
+    },
+    {   
+
+        time: '11:10-12:15 AM',
+        roomNumber: '215/216',
+        subject: 'AI(G2)/Mini Project(G1)',
+        type: 'Lab'
+    },
+      {   
+        time: '12:15-01:05 PM',
+        roomNumber: '   ',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {   
+        time: '01:05-02:10 PM',
+        roomNumber: '117',
+        subject: 'AI',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:10-03:15 PM',
+        roomNumber: '117',
+        subject: 'Soft Computing',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '08:40-10:00 AM',
+        roomNumber: '117',
+        subject: 'DAA',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:00-11:00 AM',
+        roomNumber: '117',
+        subject: 'OOSD',
+        type: 'Lecture'
+    },
+    {   
+
+        time: '11:10-12:15 AM',
+        roomNumber: '117',
+        subject: 'AI',
+        type: 'Lab'
+    },
+      {   
+        time: '12:15-01:05 PM',
+        roomNumber: '   ',
+        subject: 'Lunch',
+        type: 'Lunch'
+    },
+    {   
+        time: '01:05-02:10 PM',
+        roomNumber: '117',
+        subject: 'EITK',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:10-03:15 PM',
+        roomNumber: '117',
+        subject: 'Soft Computing',
+        type: 'Lecture'
+    }
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
